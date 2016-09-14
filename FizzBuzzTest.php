0e54514faf4e9bb11c603363963450f84591cb19
Pipekung <chanja@kku.ac.th>
2016-09-14 09:34:10 +0700

    test fizzbuzz should be one

---
+++ FizzBuzzTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class FizzBuzz
+{
+	private $NUMBER;
+	private $NUMBER_TO_TEXT = [1 => '1'];
+
+    public function __construct($number)
+    {
+		$this->NUMBER = $number;
+    }
+
+    public function toString()
+    {
+		return $this->NUMBER_TO_TEXT[$this->NUMBER];
+    }
+}
+
+class FizzBuzzTest extends PHPUnit_Framework_TestCase
+{
+    public function testFizzBuzzShouldBeOne()
+    {
+        $fizzbuzz = new FizzBuzz(1);
+        $this->assertEquals('1', $fizzbuzz->toString());
+    }
+}
